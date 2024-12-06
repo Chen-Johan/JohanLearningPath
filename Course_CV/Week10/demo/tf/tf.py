8a1b74e0e3c8e0677a9366738723e19a3f6a5c33
jhuangC <chenjiehuanghaha@gmail.com>
2024-12-07 00:46:22 +0800

    demo tf update

--- Course_CV/Week10/demo/tf/tf.py
+++ Course_CV/Week10/demo/tf/tf.py
@@ -1,6 +1,8 @@
-import tensorflow as tf
+#import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
  
 #使用numpy生成200个随机点
 x_data=np.linspace(-0.5,0.5,200)[:,np.newaxis]
